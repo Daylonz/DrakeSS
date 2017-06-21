d9693cb28da0d2a55a5664f409e979e4a0834167
Daylon <daylonjanis@gmail.com>
2017-06-21 00:55:37 -0400

    added css & images

--- src/GUI/Landing.java
+++ src/GUI/Landing.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Landing extends Application {
@@ -11,8 +12,11 @@ public class Landing extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Landing.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/Style.css");
         primaryStage.setTitle("DrakeSS - Login");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("/GUI/img/drake-dark.png"));
         primaryStage.show();
     }
 
